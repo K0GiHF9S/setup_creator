f2a38d8db78a50befd4ee2d74de6613e7476973d
K0GiHF9S <kidoairakusyunkasyuutou@gmail.com>
2021-05-08 19:01:59 +0900

    add sonarqube

--- tests/test_setting.py
+++ tests/test_setting.py
@@ -1,10 +1,7 @@
 from setup_creator.setting import Setting
-
-
 from pathlib import Path
 
 
 def test_setting(shared_datadir: Path):
     setting = Setting(shared_datadir / "test.toml")
     print(setting)
-    # assert setting.a == 1
